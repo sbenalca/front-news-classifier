4f86254e1069139d8899c69d4bfd7f8024d6a0d4
sbenalca <sbenalca@espol.edu.ec>
2023-01-16 10:56:35 -0500

    Cambios en la llamada al modelo

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const data = { username: 'example' };
 
+function setData() {
+
+    var results = JSON.parse(sessionStorage.getItem("resultados"));
+
+    if(results){
+        document.getElementById("categoria1").textContent  = results.predictions[0].clase;
+        document.getElementById("categoria2").textContent  = results.predictions[1].clase;
+        document.getElementById("categoria3").textContent  = results.predictions[2].clase;
+        document.getElementById("afavor1").textContent  += parseFloat(results.predictions[0].favor_value).toFixed(2);
+        document.getElementById("afavor2").textContent  += parseFloat(results.predictions[1].favor_value).toFixed(2);
+        document.getElementById("afavor3").textContent  += parseFloat(results.predictions[2].favor_value).toFixed(2);
+        document.getElementById("contra1").textContent  += parseFloat(results.predictions[0].contra_value).toFixed(2);
+        document.getElementById("contra2").textContent  += parseFloat(results.predictions[1].contra_value).toFixed(2);
+        document.getElementById("contra3").textContent  += parseFloat(results.predictions[2].contra_value).toFixed(2);
+        document.getElementById("imgAF1").src += results.predictions[0].favor;
+        document.getElementById("imgAF2").src += results.predictions[1].favor;
+        document.getElementById("imgAF3").src += results.predictions[2].favor;
+        document.getElementById("imgEC1").src += results.predictions[0].contra;
+        document.getElementById("imgEC2").src += results.predictions[1].contra;
+        document.getElementById("imgEC3").src += results.predictions[2].contra;
+    }    
+
+}
+
 function sendText(texto) {
     
     const data = {data: texto}
@@ -15,6 +39,10 @@ function sendText(texto) {
     })
     .then((response) => response.json())
     .then((data) => {
+
+    // Save data to sessionStorage
+    sessionStorage.setItem("resultados", JSON.stringify(data));
+
     console.log('Success:', data);
     })
     .catch((error) => {
